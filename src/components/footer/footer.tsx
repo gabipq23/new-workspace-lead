0564fdce238be445d7066243b3d962d80159be43
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2025-10-10 15:31:29 -0300

    feat: update Footer component layout for improved alignment and remove unnecessary links

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,14 +2,10 @@ function Footer() {
   return (
     <footer className="text-[#5f6368] bg-[#f7f7f7] py-4">
       <hr className="border-[#dadce0] border-t-1  mb-4" />
-      <div className="flex justify-between px-4 md:px-20 items-center  ">
+      <div className="flex justify-center px-4 md:px-20 items-center  ">
         <p style={{ margin: 0 }}>
           50.040.822/0001-74 - Gsc Solucoes Corporativas
         </p>
-        <div className="flex gap-6">
-          <p style={{ margin: 0 }}>Privacidade</p>
-          <p style={{ margin: 0 }}>Termos</p>
-        </div>
       </div>
     </footer>
   );
